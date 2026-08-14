1c80a8efc69b470993e883bb76e0b40f907a0206
nguoidungtab805-cell <nguoidungtab805@gmail.com>
2026-08-14 18:37:10 +0700

    Create useAuth.ts

---
+++ hooks/useAuth.ts
@@ -0,0 +1,19 @@
+"use client";
+import { useEffect, useState } from "react";
+import { auth } from "../firebase/firebaseClient";
+import { onAuthStateChanged, User } from "firebase/auth";
+
+export function useAuth() {
+  const [user, setUser] = useState<User | null | undefined>(undefined);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (u) => {
+      setUser(u);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return { user, loading };
+}
